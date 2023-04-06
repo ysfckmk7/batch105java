5d67ecf84a76a5cfb9ab8198dd90290000de4ce1
yusuf <ysfckmk7@gmail.com>
2023-04-06 13:01:20 +0300

    first commit

--- src/ders01_programayaGiriş/C01_HelloWorld.java
+++ src/ders01_programayaGiriş/C01_HelloWorld.java
@@ -1,5 +1,7 @@
 package ders01_programayaGiriş;
 
+import java.lang.reflect.Type;
+
 public class C01_HelloWorld {
 
 
@@ -7,7 +9,16 @@ public class C01_HelloWorld {
 
         // dunyasına merhaba dıyelım
 
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
+
+        String asd="1.907 TL";
+        asd=asd.replaceAll("\\D","");
+        String a="1907";
+       int f=Integer.parseInt(asd);
+        System.out.println(f);
+
+
+
 
 
     }
